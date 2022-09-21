10b297e3ad8d9f2cc37980c07a2601aa0f014e89
Manuel Fernandez <manufer6503@gmail.com>
2022-09-21 00:53:31 -0300

    Classes & Functions
    Two classes were created. One is called "Items" and represents the products. The other is called "Storage" and represents any generic kind of storage (e.g. cart and store are Storage instances).
    The UX was improved by adding functions who's execute when onclick events are listened. This functions allows to interact with stock property of the items inside the storages.

--- src/displayCart.js
+++ src/displayCart.js
@@ -1,58 +1,144 @@
+// ==================== TAREA ====================
+//Deshabilitar el boton de agregar una vez que el producto ya este en el carrito
+//Idea: Usar funcion distinta a "addOrRemoveFromCart()" en el evento onclick del boton Agregar
+
+//El boton de restar cantidad en el carrito debe detenerse una vez llegue a cero y no eliminar el elemento del cart
+
+//Ocultar el elemento html que contiene el titulo del carrito si cart está vacio
+
+//Agregar :hover a los botones
+
+
+
+
+
+
+
+
+
+
+// ==================== CLASS ====================
+
+//CUANDO MODULARIZE EL CODIGO ACA VAN LOS IMPORTS DE LAS CLASSES
+//Las clases se suelen almacenar en ficheros individuales.
+//De forma que cada clase que creamos. debería estar en un fichero con su mismo nombre.
+//Ej.: Item.js tiene adentro la class Item
+
+//Representa productos
+class Item {
+	constructor(name, id, price, stock, desc, img) {
+		this.name = name;
+		this.id = id;
+		this.price = price;
+		this.stock = stock;
+		this.desc = desc;
+		this.img = img;
+	}
+
+	checkStock(amount) {
+		if(this.stock >= amount) {
+			return true
+		}
+		return false
+	}
+
+	calcSubtotal() {
+		return this.price * this.stock;
+	}
+}
+
+//Representa almacenamiento de Items.
+class Storage extends Array {
+	findProduct(IdProduct) {
+		return this.find(product => product.id == IdProduct);
+	}
+
+	createProduct(product) {
+		this.push(new Item(product.name, product.id, product.price, 1, product.desc, product.img));
+	}
+
+	moveProductStockFromThisTo(IdProduct, amount, storageToUpdate) {
+		let product = this.findProduct(IdProduct);
+
+		if(product.checkStock(amount)) {
+			product.stock -= amount;
+
+			if(storageToUpdate.findProduct(IdProduct) === undefined) {
+				storageToUpdate.createProduct(product);
+				storageToUpdate.findProduct(product.id).stock = amount;
+			} else {
+				storageToUpdate.findProduct(product.id).stock += amount;	
+			}
+
+			console.log('el valor fue actualizado');
+		} else {
+			console.log('no hay stock');
+		}
+	}
+
+	cleanStorage() {
+		this.forEach(product => {
+
+			if(product.stock === 0) {
+				let index = this.indexOf(product);
+				this.splice(index, 1);
+			}
+		});
+	}
+
+	calcTotal() {
+		let total = 0;
+
+		this.forEach(product => total += product.calcSubtotal());
+		return total;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+// ==================== VARIABLES ====================
 
 //Referencias a los elementos del DOM
 let shop = document.getElementById('shop');
 let cartList = document.getElementById('cartList');
 
-//Arrays de ejemplo
-let store = [
-	{
-		name: 'Café',
-		price: 30,
-		stock: 10,
-		input: '1',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		img:'../src/assets/img/cards_img/coffee.jpg'
-	},
-	{
-		name: 'Jugo',
-		price: 25,
-		stock: 10,
-		input: '2',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		img:'../src/assets/img/cards_img/juice.jpg'
-	},
-	{
-		name: 'Medialuna',
-		price: 15,
-		stock: 10,
-		input: '3',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		img:'../src/assets/img/cards_img/medialuna.jpg'
-	},
-	{
-		name: 'Sandwich',
-		price: 35,
-		stock: 10,
-		input: '4',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-		img:'../src/assets/img/cards_img/sandwich.jpg'
-	}
-];
+let genericDescription = '1 Lorem ipsum dolor sit amet, consectetur adipisicing.';
 
-let cart = [
-	{
-		name: 'Sandwich',
-		price: 35,
-		stock: 5,
-		img:'../src/assets/img/cards_img/sandwich.jpg'
-	},
-	{
-		name: 'Café',
-		price: 30,
-		stock: 3,
-		img:'../src/assets/img/cards_img/coffee.jpg'
+let store = new Storage(
+	new Item('Café', '1', 15, 10, genericDescription, '../src/assets/img/cards_img/coffee.jpg'),
+	new Item('Jugo', '2', 20, 10, genericDescription, '../src/assets/img/cards_img/juice.jpg'),
+	new Item('Medialuna', '3', 20, 10, genericDescription, '../src/assets/img/cards_img/medialuna.jpg'),
+	new Item('Sandwich', '4', 30, 10, genericDescription, '../src/assets/img/cards_img/sandwich.jpg')
+);
+
+let cart = new Storage();
+
+
+
+
+
+
+
+
+
+
+// ==================== FUNCIONES ====================
+
+//Devuelve el valor absoluto de un numero
+function modulo(number) {
+	if(number >= 0) {
+		return number;
+	} else {
+		return -number;
 	}
-];
+}
 
 
 //Genera las cards de todos los productos del array 'sotre' por medio del método map
@@ -67,7 +153,7 @@ function generateShop() {
 					<p class="card-text font__300">${product.desc}</p>
 					<div class="d-flex justify-content-between">
 						<p class="h5 fw-semibold m-0 karla">$${product.price}</p>
-						<button class="h5 p-2 text-uppercase button__cart paytoneone">Agregar</button>
+						<button onclick="addOrRemoveFromCart(${product.id}, true)" class="h5 p-2 text-uppercase button__cart paytoneone">Agregar</button>
 					</div>
 				</div>
 			</div>
@@ -75,6 +161,7 @@ function generateShop() {
 		`
 	}).join("");
 }
+
 
 
 //Genera las cards de los elementos del array 'cart' iterando a traves de sus elementos con un forOf
@@ -91,21 +178,57 @@ function generateCart() {
 							</div>
 						</div>
 						<div class="col-4 offset-4 col-lg-1 my-3 p-2 offset-lg-3 my-lg-auto d-flex justify-content-evenly align-items-center border border-dark">
-								<button id="increment_btn_${product.input}" class="m-0 h4 bg-transparent border-0 karla font__400">-</button>
+								<button onclick="addOrRemoveFromCart(${product.id}, false)" class="m-0 h4 bg-transparent border-0 karla font__400">-</button>
 								<p class="m-0 h4 karla font__400">${product.stock}</p>
-								<button id="decrement_btn_${product.input}" class="m-0 h4 bg-transparent border-0 karla font__300">+</button>					
+								<button onclick="addOrRemoveFromCart(${product.id}, true)" class="m-0 h4 bg-transparent border-0 karla font__300">+</button>					
 						</div>
 						<div class="col-lg-2 m-auto d-flex justify-content-center justify-content-lg-start">
-							<p class="ms-2 mb-3 h4 karla font__400">$175</p>
+							<p class="ms-2 mb-3 h4 karla font__400">$${product.calcSubtotal()}</p>
 						</div>
 						<div class="col-lg-1 m-auto d-flex justify-content-center">
-							<button class="p-3 border border-danger text-danger text-uppercase karla bg-transparent">Eliminar</button>
+							<button onclick="eraseProductFromCart(${product.id})" class="p-3 border border-danger text-danger text-uppercase karla bg-transparent">Eliminar</button>
 						</div>`;
 		cartList.appendChild(row);
 	}
+	let totalRow = document.createElement('div');
+	totalRow.className = 'container row mx-auto mb-3 py-2 border-top align-items-center justify-content-between';
+	totalRow.innerHTML = `<p class="col-1 h3 paytoneone">Total</p>
+						<p class="col-1 display-6 karla">$${cart.calcTotal()}</p>`;
+
+	cartList.appendChild(totalRow);
 }
 
 
-//Invoco funciones
-generateShop();
-generateCart();
+
+function refreshCartList() {
+	cartList.innerHTML = '';
+	cart.cleanStorage();
+	generateCart();
+}
+
+
+
+function addOrRemoveFromCart(IdProduct, operator) {
+	let operation = operator ? 1 : -1;
+
+	store.moveProductStockFromThisTo(IdProduct, operation, cart);
+	refreshCartList();
+}
+
+
+
+function eraseProductFromCart(IdProduct) {
+	let amount = cart.findProduct(IdProduct).stock;
+	cart.moveProductStockFromThisTo(IdProduct, amount, store);
+	cart.cleanStorage();
+	refreshCartList();
+}
+
+
+
+function init() {
+	generateShop();
+	refreshCartList();
+}
+
+init();
