11e848650b5eca27b9451870f1437340cbb96496
Manuel Fernandez <manufer6503@gmail.com>
2022-09-26 23:28:21 -0300

    Delete folder services & change js file name

--- src/services/localStorage.js
+++
@@ -1,7 +0,0 @@
-export function updateLocalStorage(cart) {
-	localStorage.setItem('user_cart', JSON.stringify(cart));
-}
-
-export function getLocalStorage() {
-	return JSON.parse(localStorage.getItem('user_cart'));
-}
