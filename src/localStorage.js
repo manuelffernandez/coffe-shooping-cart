aba5f72fe75efc57240da0290669fdb4d6cfda94
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-10-13 23:09:00 -0300

    create localStorage file

---
+++ src/localStorage.js
@@ -0,0 +1,9 @@
+export function updateLocalStorageCart(cart) {
+	localStorage.setItem('user_cart', JSON.stringify(cart));
+}
+
+
+
+export function getCartFromLocalStorage() {
+	return JSON.parse(localStorage.getItem('user_cart'));
+}
