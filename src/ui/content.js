0bb3266bb3a27e49555c35f7e48654cd50c2e006
Manuel Fernandez <manufer6503@gmail.com>
2022-10-26 15:47:20 -0300

    creates generateModal func

--- src/ui/content.js
+++ src/ui/content.js
@@ -1,3 +1,46 @@
+// ======================================================
+// ==================== purchHistory ====================
+// ======================================================
+function generatePurchaseModal(purchase) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.id = 'exampleModal';
+    modal.tabIndex = '-1'
+    modal.setAttribute('aria-labelledby', 'exampleModalLabel')
+    modal.setAttribute('aria-hidden', 'true')
+    // modal.ariaLabel = 'exampleModalLabel'
+    // modal.ariaHidden = 'true';
+
+    modal.innerHTML = `<div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            ...
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+        </div>`;
+
+    console.log(modal);
+    return modal
+}
+
+function generateAllPurchaseModals(record) {
+    let allModals = document.createElement('div');
+
+    for(let purchase of record) {
+        allModals.appendChild(generatePurchaseModal(purchase))
+    }
+
+    return allModals
+}
+
 function generateTableStructure() {
     let table = `<thead>
                     <tr>
@@ -19,7 +62,7 @@ function generateTableRow(purchase) {
                     <td>${purchase.orderId}</td>
                     <td>$${purchase.amount}</td>
                     <td>
-                        <button id="" class="bg-transparent border-0 text-info link">Ver detalle</button>
+                        <button id="" class="bg-transparent border-0 text-info link" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver detalle</button>
                     </td>`
     return row
 }
@@ -32,6 +75,10 @@ function generateAllTableRows(record) {
     }
 }
 
-const purchaseContent = { generateAllTableRows, generateTableStructure };
+const htmlGenerator = {
+    generateAllTableRows,
+    generateTableStructure,
+    generateAllPurchaseModals
+}
 
-export default purchaseContent;
+export default htmlGenerator;
