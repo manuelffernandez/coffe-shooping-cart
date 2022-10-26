980a761e2bc1df4bd69fbfcfa281140530703b79
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-10-26 19:42:51 -0300

    clean syntax

--- src/purchHistory.js
+++ src/purchHistory.js
@@ -2,12 +2,8 @@ import services from "./services/services.js";
 import ui from "./ui/ui.js";
 
 let record;
-const table = document.querySelector('#purchase-table');
 const modals = document.querySelector('#modal-container');
-console.log(modals);
-const purchButtonsFunctionsList = {
-
-}
+const table = document.querySelector('#purchase-table');
 
 async function initHistory() {
     ui.showLoadingAlert('Cargando historial de compras')
