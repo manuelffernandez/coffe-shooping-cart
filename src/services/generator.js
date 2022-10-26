1fdd2f79a28001fd31386d00a9e625332878291e
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-10-26 01:07:24 -0300

    add total property to purchase object

--- src/services/generator.js
+++ src/services/generator.js
@@ -1,4 +1,5 @@
 export default function generatePurchaseObject(cart) {
+    const total = cart.calcTotal();
     let auxiliarCart = [...cart];
     let purchaseDate = new Date();
     let purchase = {};
@@ -7,6 +8,7 @@ export default function generatePurchaseObject(cart) {
         delete product.img;
         delete product.desc;
     }
+    purchase.amount = total;
     purchase.products = {...auxiliarCart};
     purchase.date = `${purchaseDate.getDay()}-${purchaseDate.getMonth()}-${purchaseDate.getFullYear()}`;
     purchase.orderId = Math.floor(Math.random() * 50000);
