b93fff153f0895c9473ce21f619182bf69bc4673
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-10-26 01:27:32 -0300

    ass TODO item

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 // ============================================
 // TODO: Agregar comentario en imagenes.svg explicando de que se trata el archivo
 // TODO: Generar el codigo en cartContainer dentro de la funcion ./ui/ui.js/generateCart() como en purchHistory.js
+// TODO: Separar ui.js en un modulo que solo genere codigo html.
 
 // =================================================
 // ==================== IMPORTS ====================
