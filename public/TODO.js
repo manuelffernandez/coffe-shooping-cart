6eaace31414844c2076adf8caeb46aca4b56b6a5
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-10-28 18:12:57 -0300

    check TODO item & cleans style.css syntax

--- public/TODO.js
+++ public/TODO.js
@@ -3,4 +3,4 @@
 // ============================================
 // TODO: Generar el codigo en cartContainer dentro de la funcion ./ui/ui.js/generateCart() como en purchHistory.js
 // TODO: Separar ui.js en un modulo que solo genere codigo html.
-// TODO: Agregar funcionabilidad a boton "Ver detalle" en el historial de compras
+// TODO: Hacer una página version inglés
